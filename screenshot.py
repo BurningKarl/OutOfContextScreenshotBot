20640c973164041d7ed2954f78a4374e75075584
BurningKarl <10158964+BurningKarl@users.noreply.github.com>
2020-09-03 17:18:53 +0200

    Added check for invalid lobby ids

--- screenshot.py
+++ screenshot.py
@@ -3,8 +3,15 @@ import time
 from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import (
+    NoSuchElementException as PageDesignChangedException,
+)
 
 OUTOFCONTEXT_BASE_URL = "https://www.outofcontext.party/lobby/"
+
+
+class InvalidLobbyException(Exception):
+    pass
 
 
 def save_outofcontext_screenshot(lobby):
@@ -16,16 +23,25 @@ def save_outofcontext_screenshot(lobby):
     chrome_options.add_argument("--start-maximized")
     driver = webdriver.Chrome(options=chrome_options)
 
-    # Set the username
+    # Load the webpage
     driver.get(url)
     time.sleep(1)
+
+    # Check the validity of the lobby id
+    title_div = driver.find_element_by_class_name("title")
+    if title_div.text == "Invalid Lobby":
+        raise InvalidLobbyException(f"The lobby {lobby!r} does not exist")
+
+    # Set the username
     driver.find_element_by_name("playerName").send_keys("ScreenshotBot")
     driver.find_element("xpath", "//button").click()
     time.sleep(1)
 
-    # Take the screenshot
-    page_element = driver.find_elements_by_class_name("page")[0]
+    # Resize the browser window
+    page_element = driver.find_element_by_class_name("page")
     driver.set_window_size(page_element.size["width"], page_element.size["height"] + 50)
+
+    # Take the screenshot
     menu_element = driver.find_elements_by_class_name("menu")[0]
     filename = driver.title + "_" + datetime.now().isoformat() + ".png"
     menu_element.screenshot(filename)
